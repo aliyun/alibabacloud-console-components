d256c384a3b9eb489799a036d2ffbd428591c63c
csr632 <632882184@qq.com>
2019-11-13 16:16:12 +0800

    fix(component/form): fix labelTextAlign="right"

--- packages/component/src/components/form/demo/demo1.js
+++ packages/component/src/components/form/demo/demo1.js
@@ -5,33 +5,38 @@ const FormItem = Form.Item
 
 const formItemLayout = {
   labelCol: {
-    fixedSpan: 10
+    fixedSpan: 10,
   },
   wrapperCol: {
-    span: 14
-  }
+    span: 14,
+  },
 }
 
 export default class Demo1 extends React.Component {
-
-  handleSubmit = (values) => {
-    console.log('Get form value:', values);
+  handleSubmit = values => {
+    console.log('Get form value:', values)
   }
 
   render() {
     return (
-      <Form style={{width: '60%'}} {...formItemLayout} >
+      <Form style={{ width: '60%' }} {...formItemLayout}>
         <FormItem label="userName:">
           <p>Fixed Name</p>
         </FormItem>
         <FormItem label="password:">
-          <Input htmlType="password" name="pass" placeholder="Please Enter Password"/>
+          <Input
+            htmlType="password"
+            name="pass"
+            placeholder="Please Enter Password"
+          />
         </FormItem>
         <FormItem label="Note:" help="something">
           <Input.TextArea placeholder="something" name="remark" />
         </FormItem>
         <FormItem label="Agreement:">
-          <Checkbox name="agreement" defaultChecked>Agree</Checkbox>
+          <Checkbox name="agreement" defaultChecked>
+            Agree
+          </Checkbox>
         </FormItem>
         <FormItem label=" ">
           <Form.Submit onClick={this.handleSubmit}>Confirm</Form.Submit>
--- packages/component/src/components/form/index.js
+++ packages/component/src/components/form/index.js
@@ -1,4 +1,35 @@
+import * as React from 'react'
 import Form from '@alifd/next/lib/form'
 import './index.scss'
+import hoistStatics from 'hoist-non-react-statics'
 
-export default Form
+const { Item } = Form
+
+const PropsCtx = React.createContext()
+
+const WrappedForm = props => {
+  return (
+    <PropsCtx.Provider value={props}>
+      <Form {...props} />
+    </PropsCtx.Provider>
+  )
+}
+
+const WrappedItem = props => {
+  return (
+    <PropsCtx.Consumer>
+      {formProps => {
+        // fusion默认label靠右对齐，wind希望默认靠左对齐
+        const labelTextAlign =
+          formProps.labelTextAlign === 'right' ? 'right' : 'left'
+        return <Item labelTextAlign={labelTextAlign} {...props} />
+      }}
+    </PropsCtx.Consumer>
+  )
+}
+
+hoistStatics(WrappedForm, Form)
+hoistStatics(WrappedItem, Item)
+WrappedForm.Item = WrappedItem
+
+export default WrappedForm
