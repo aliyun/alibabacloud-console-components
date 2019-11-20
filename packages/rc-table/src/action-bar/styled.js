60a2146ab2dcb5260947ca25c62254fdbf28b687
李明阳 <1347276498@qq.com>
2019-11-20 16:24:40 +0800

    chore(rc-table): add styled-components
    Co-authored-by: csr632 <632882184@qq.com>

---
+++ packages/rc-table/src/action-bar/styled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+import { Grid } from '@alicloud/console-components'
+
+export const SActionBarWrapper = styled(Grid.Row)`
+  height: 40px;
+  justify-content: space-between;
+`
+
+export const SActionBarLeft = styled(Grid.Col)`
+  flex: 0 1 auto;
+
+  > * {
+    display: inline-block;
+    margin-right: 8px;
+  }
+`
+
+export const SActionBarRight = styled(Grid.Col)`
+  flex: 0 1 auto;
+
+  > * {
+    margin-right: 0;
+    margin-left: 8px;
+  }
+`
+
+export const SFixedBarWrapper = styled.div`
+  position: sticky;
+  display: block;
+  background: #fff;
+  width: 100%;
+  left: 0;
+
+  &.fixed-to-top {
+    top: 0;
+    padding-top: 8px;
+    border-bottom: 1px solid #dedede;
+    box-shadow: 0px 0.2px 10px #ccc;
+  }
+
+  &.fixed-to-bottom {
+    bottom: 0;
+    padding-bottom: 8px;
+    border-top: 1px solid #dedede;
+    box-shadow: 0px 0.2px 10px #ccc;
+  }
+`
