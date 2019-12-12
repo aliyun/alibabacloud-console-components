192d3d337a5766a4fed6eed4c8ee8be530224cfb
李明阳 <1347276498@qq.com>
2019-12-12 23:04:28 +0800

    fix(component/message): fix toast style
    replace toast icon & reduce right padding

---
+++ packages/component/src/components/message/iconMap.js
@@ -0,0 +1,10 @@
+const iconMap = {
+  error: 'delete-filling',
+  success: 'success-filling',
+  warning: 'warning1',
+  notice: 'info-circle-fill',
+  info: 'info-circle-fill',
+  help: 'question-circle-fill',
+}
+
+export default iconMap
