4c7d61c64f23580493b7d08075f8896e04c98f33
csr632 <632882184@qq.com>
2020-01-15 20:56:09 +0800

    chore(rc-page): make rc-page use dev-kit

--- packages/rc-page/breezr.config.js
+++ packages/rc-page/breezr.config.js
@@ -6,7 +6,12 @@ module.exports = {
         moduleName: 'WindRcPage',
         useTypescript: true,
         externals: {
-          '@alicloud/console-components': 'wind',
+          '@alicloud/console-components': {
+            root: 'wind',
+            commonjs2: '@alicloud/console-components',
+            commonjs: '@alicloud/console-components',
+            amd: '@alicloud/console-components'
+          },
         },
       },
     ],
