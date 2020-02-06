9aa70aa9980da552228e260a0fe7003103049422
李明阳 <1347276498@qq.com>
2020-02-06 15:06:38 +0800

    fix(component/menu-button): adjust icon color when menu-button attribute is text
    fix: https://work.aone.alibaba-inc.com/issue/24845125

--- packages/component/src/components/menu-button/demo/demo1.js
+++ packages/component/src/components/menu-button/demo/demo1.js
@@ -2,21 +2,34 @@ import React from 'react'
 import { MenuButton } from '@alicloud/console-components'
 
 const { Item } = MenuButton
-const menu = ['Undo', 'Redo', 'Cut', 'Copy', 'Paste'].map(item => <Item key={item}>{item}</Item>)
+const menu = ['Undo', 'Redo', 'Cut', 'Copy', 'Paste'].map(item => (
+  <Item key={item}>{item}</Item>
+))
 
 const Demo1 = () => (
   <div>
     <MenuButton label="Document Edit">{menu}</MenuButton>
     &nbsp;&nbsp;
-    <MenuButton type="primary" label="Document Edit">{menu}</MenuButton>
+    <MenuButton type="primary" label="Document Edit">
+      {menu}
+    </MenuButton>
     &nbsp;&nbsp;
-    <MenuButton type="secondary" label="Document Edit">{menu}</MenuButton>
-    <br /><br />
-    <MenuButton text label="Document Edit">{menu}</MenuButton>
+    <MenuButton type="secondary" label="Document Edit">
+      {menu}
+    </MenuButton>
+    <br />
+    <br />
+    <MenuButton text label="Document Edit">
+      {menu}
+    </MenuButton>
     &nbsp;&nbsp;
-    <MenuButton text type="primary" label="Document Edit">{menu}</MenuButton>
+    <MenuButton text type="primary" label="Document Edit">
+      {menu}
+    </MenuButton>
     &nbsp;&nbsp;
-    <MenuButton text type="secondary" label="Document Edit">{menu}</MenuButton>
+    <MenuButton text type="secondary" label="Document Edit">
+      {menu}
+    </MenuButton>
   </div>
 )
 
