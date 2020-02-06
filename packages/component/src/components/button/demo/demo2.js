8c62b758f65ed874e9c825b6ed7f7b55479608b5
李明阳 <1347276498@qq.com>
2020-02-06 11:37:50 +0800

    fix(component/button): adjust button`s demo to fit design
    fix: https://work.aone.alibaba-inc.com/issue/24788991

--- packages/component/src/components/button/demo/demo2.js
+++ packages/component/src/components/button/demo/demo2.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Button } from '@alicloud/console-components'
+import Styled from 'styled-components'
 import './demo2.less'
 
 const Demo2 = () => (
-  <div className="button-demo-size-list">
+  <Wrapper>
     <Button size="small">小型按钮</Button>
     <Button size="medium">中型按钮</Button>
     <Button size="large">大型按钮</Button>
-  </div>
+  </Wrapper>
 )
+
+const Wrapper = Styled.div`
+  .next-btn {
+    margin-right: 8px;
+  }
+`
 
 export default Demo2
