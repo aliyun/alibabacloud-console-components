5f208bfa7fc6a9456be01fcf85958f4643b6cdf9
李明阳 <1347276498@qq.com>
2020-02-12 14:50:55 +0800

    fix(component/overlay): adjust overlay style to fit design
    1. add box-shadow
    2. change border color
    3. fix: https://work.aone.alibaba-inc.com/issue/24806869

--- packages/component/src/components/overlay/demo/demo3.js
+++ packages/component/src/components/overlay/demo/demo3.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Overlay } from '@alicloud/console-components'
+import styled from 'styled-components'
 
 const { Popup } = Overlay
 
 const Demo3 = () => (
   <Popup trigger={<button>Open</button>} triggerType="click">
-    <span className="overlay-demo">
-      Hello World From Popup!
-    </span>
+    <SInner>Hello World From Popup!</SInner>
   </Popup>
 )
+
+const SInner = styled.div`
+  width: 300px;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid #999999;
+  background: #ffffff;
+  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.15);
+`
 
 export default Demo3
--- packages/component/src/components/overlay/demo/demo5.js
+++ packages/component/src/components/overlay/demo/demo5.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Overlay } from '@alicloud/console-components'
+import styled from 'styled-components'
 
 const { Popup } = Overlay
 
 const Demo5 = () => (
-  <Popup trigger={<button>Open first overlay</button>}
-    triggerType="click">
-    <div className="overlay-demo">
-      <Popup trigger={<button>Open second overlay</button>}
+  <Popup trigger={<button>Open first overlay</button>} triggerType="click">
+    <SInner>
+      <Popup
+        trigger={<button>Open second overlay</button>}
         triggerType="click"
-        container={trigger => trigger.parentNode}>
-        <div className="overlay-demo">
+        container={trigger => trigger.parentNode}
+      >
+        <SInner>
           <p>Hello World From Second Overlay!</p>
-        </div>
+        </SInner>
       </Popup>
       <p>Hello World From First Overlay!</p>
-    </div>
+    </SInner>
   </Popup>
 )
+
+const SInner = styled.div`
+  width: 300px;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid #999999;
+  background: #ffffff;
+  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.15);
+`
 
 export default Demo5
