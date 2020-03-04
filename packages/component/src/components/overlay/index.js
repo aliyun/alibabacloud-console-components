5612ec1e4e062f8275b7b23dda36c7810f22a0e6
csr632 <632882184@qq.com>
2020-03-04 17:00:24 +0800

    fix(component/overlay): overlay should not provide border

--- packages/component/src/components/overlay/index.js
+++ packages/component/src/components/overlay/index.js
@@ -1,28 +1,4 @@
 import Overlay from '@alifd/next/lib/overlay'
-import React from 'react'
-import cs from 'classnames'
-import hoistStatics from 'hoist-non-react-statics'
 import './index.scss'
 
-const { Popup } = Overlay
-
-const WrappedOverlay = ({ wrapperClassName, ...props }) => (
-  <Overlay
-    {...props}
-    wrapperClassName={cs('custom-overlay-wrapper', wrapperClassName)}
-  />
-)
-
-const WrappedPopup = ({ wrapperClassName, ...props }) => (
-  <Popup
-    {...props}
-    wrapperClassName={cs('custom-overlay-wrapper', wrapperClassName)}
-  />
-)
-
-hoistStatics(WrappedOverlay, Overlay)
-hoistStatics(WrappedPopup, Popup)
-
-WrappedOverlay.Popup = WrappedPopup
-
-export default WrappedOverlay
+export default Overlay
