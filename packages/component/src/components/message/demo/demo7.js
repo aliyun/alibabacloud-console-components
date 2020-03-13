c0255f6941378bdf878ea480560d5a35b58a588a
李明阳 <1347276498@qq.com>
2020-03-13 15:18:27 +0800

    fix(component/message): use original Toast, remove custom Toast

--- packages/component/src/components/message/demo/demo7.js
+++ packages/component/src/components/message/demo/demo7.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Message, Button } from '@alicloud/console-components'
 import './demo7.less'
 
-const showSuccess = () => Message.success('success')
+const showSuccess = () =>
+  Message.success({
+    title: 'success',
+    duration: 3000,
+  })
 const showWarning = () => Message.warning('warning')
 const showError = () => Message.error('error')
 const showNotice = () => Message.notice('notice')
