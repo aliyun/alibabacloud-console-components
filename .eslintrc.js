360b16d0f63366c757f95bd5b0bb0f891d56bce5
csr632 <632882184@qq.com>
2020-03-26 16:39:20 +0800

    fix: fix eslint config to support mixing js and ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
   root: true,
-  extends: ['@alicloud/eslint-config-console-components'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.eslint.json',
-  },
-  rules: {
-    'no-underscore-dangle': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'no-plusplus': 'off',
-    'react/static-property-placement': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: ['@alicloud/eslint-config-console-components/ts'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.eslint.json',
+      },
+      rules: {
+        'no-underscore-dangle': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'no-plusplus': 'off',
+        'react/static-property-placement': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      extends: ['@alicloud/eslint-config-console-components/js'],
+    },
+  ],
 }
--- packages/eslint-config/js.js
+++ packages/eslint-config/js.js
@@ -1,34 +1,9 @@
 module.exports = {
-  extends: ['eslint-config-ali/react'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 7,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      experimentalDecorators: true,
-      jsx: true,
-    },
-  },
-  rules: {
-    'max-len': [
-      'warn',
-      {
-        code: 80,
-        tabWidth: 2,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
-    'no-console': [
-      'warn',
-      {
-        allow: ['warn', 'error'],
-      },
-    ],
-    semi: 0,
-    'react/jsx-max-props-per-line': ['warn', { maximum: 1, when: 'always' }],
-    'react/jsx-indent-props': ['warn', 2],
-  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/react',
+  ],
 }
