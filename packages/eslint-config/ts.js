d7e3f076a4d3a5c4f99596e2f14600f92eed256c
csr632 <632882184@qq.com>
2020-04-02 12:06:16 +0800

    chore(eslint-config): remove unnecessary configs

--- packages/eslint-config/ts.js
+++ packages/eslint-config/ts.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
   parser: '@typescript-eslint/parser',
   extends: [
+    // https://github.com/iamturns/eslint-config-airbnb-typescript#i-wish-this-config-would-support-
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -12,16 +12,11 @@ module.exports = {
     'prettier/@typescript-eslint',
     'prettier/react',
   ],
-  plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
-    // https://reactjs.org/docs/hooks-rules.html#eslint-plugin
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': [
       'error',
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    // 'react/destructuring-assignment': 'off',
     'react/require-default-props': 'off',
     'consistent-return': 'off',
     'import/no-extraneous-dependencies': 'off',
@@ -34,23 +29,12 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'react/prop-types': 'off',
-    // https://stackoverflow.com/a/59268871
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
   },
   // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md#configuring-in-a-mixed-jsts-codebase
   overrides: [
     {
       // enable the rule specifically for TypeScript files
-      files: ['*.ts', '*.tsx'],
+      files: ['**/*.ts', '**/*.tsx'],
       rules: {
         '@typescript-eslint/interface-name-prefix': ['error', 'always'],
         '@typescript-eslint/no-use-before-define': [
@@ -68,14 +52,6 @@ module.exports = {
       },
     },
   ],
-  settings: {
-    // https://github.com/kriasoft/react-starter-kit/issues/1180#issuecomment-436753540
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
   env: {
     // https://stackoverflow.com/q/42377038
     browser: true,
