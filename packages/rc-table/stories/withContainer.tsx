cee2870ba201e31849ef1247cbfbeea058180b4c
csr632 <632882184@qq.com>
2020-04-24 16:09:04 +0800

    chore(rc-table): improve demo and doc

--- packages/rc-table/stories/withContainer.tsx
+++ packages/rc-table/stories/withContainer.tsx
@@ -55,81 +55,91 @@ const secondaryOperation = () => () => (
 
 const App: React.FC<{
   creatable?: boolean
-}> = props => {
+}> = (props) => {
   const targetRef = useRef<HTMLDivElement | null>(null)
   const containerRef = useRef<HTMLDivElement | null>(null)
 
   return (
+    // 顶栏、底栏吸附的时候，DOM节点会挂载在它下面
     <SContainer ref={containerRef}>
-      <SWrapper ref={targetRef}>
-        <Table
-          exact
-          affixActionBar
-          fixedBarZIndex={1000}
-          affixBarOverlayProps={{
-            target: () => targetRef.current,
-            container: () => containerRef.current,
-          }}
-          afterFixedBarIntersectChanged={(
-            alignType: 'top' | 'bottom',
-            isIntersecting: boolean
-          ) => {
-            console.log(
-              'alignType:',
-              alignType,
-              'isIntersecting:',
-              isIntersecting
-            )
-          }}
-          dataSource={dataSource}
-          columns={columns}
-          rowSelection={rowSelection}
-          primaryKey="id"
-          operation={{
-            primary: primaryOperation(props),
-            secondary: secondaryOperation(),
-          }}
-          search={{
-            filter: [
-              {
-                value: 'InstanceName',
-                label: 'By Instance Name',
-              },
-            ],
-          }}
-          pagination={{
-            current: 1,
-            total: 40,
-            pageSize: 10,
-          }}
-          selection={({ selectedRowKeys }: { selectedRowKeys: any[] }) => {
-            return (
-              <>
-                <Badge count={selectedRowKeys.length}>
-                  <Button disabled={selectedRowKeys.length === 0}>
-                    Delete
-                  </Button>
-                </Badge>
-              </>
-            )
-          }}
-        />
-      </SWrapper>
+      {/* SScroll是滚动的容器 */}
+      <SScroll ref={targetRef}>
+        <SWidthControl>
+          {Array.from(Array(50)).map((_, idx) => (
+            <p>这是表格前面的内容{idx}</p>
+          ))}
+          <Table
+            exact
+            affixActionBar
+            fixedBarZIndex={1000}
+            affixBarOverlayProps={{
+              target: () => targetRef.current,
+              container: () => containerRef.current,
+            }}
+            afterFixedBarIntersectChanged={(
+              alignType: 'top' | 'bottom',
+              isIntersecting: boolean
+            ) => {
+              console.log(
+                'alignType:',
+                alignType,
+                'isIntersecting:',
+                isIntersecting
+              )
+            }}
+            dataSource={dataSource}
+            columns={columns}
+            rowSelection={rowSelection}
+            primaryKey="id"
+            operation={{
+              primary: primaryOperation(props),
+              secondary: secondaryOperation(),
+            }}
+            search={{
+              filter: [
+                {
+                  value: 'InstanceName',
+                  label: 'By Instance Name',
+                },
+              ],
+            }}
+            pagination={{
+              current: 1,
+              total: 40,
+              pageSize: 10,
+            }}
+            selection={({ selectedRowKeys }: { selectedRowKeys: any[] }) => {
+              return (
+                <>
+                  <Badge count={selectedRowKeys.length}>
+                    <Button disabled={selectedRowKeys.length === 0}>
+                      Delete
+                    </Button>
+                  </Badge>
+                </>
+              )
+            }}
+          />
+          {Array.from(Array(50)).map((_, idx) => (
+            <p>这是表格后面的内容{idx}</p>
+          ))}
+        </SWidthControl>
+      </SScroll>
     </SContainer>
   )
 }
 
 const SContainer = styled.div`
   position: relative;
-  width: 100%;
 `
 
-const SWrapper = styled.div`
-  height: calc(100vh);
-  padding: 0 30px;
-  position: relative;
+const SScroll = styled.div`
+  height: 600px;
   overflow-y: auto;
-  width: 100%;
+  /* 控制内容的宽度 */
+  padding: 0 48px;
 `
+
+const SWidthControl = styled.div``
 
 export default App
