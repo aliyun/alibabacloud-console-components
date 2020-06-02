b6127d3ffbd9fa44eeb29f24701a372eaccd154f
csr632 <632882184@qq.com>
2020-06-02 19:03:20 +0800

    fix(rc-data-fields): provide displayName to styled-components
    to avoid styled conflict with aliyun topbar

--- packages/rc-data-fields/src/styles/index.tsx
+++ packages/rc-data-fields/src/styles/index.tsx
@@ -1,18 +1,25 @@
+import React from 'react'
 import styled from 'styled-components'
 import { Grid } from '@alicloud/console-components'
 
 const { Col } = Grid
 
-export const SFieldItem = styled(Col)`
+const WindCol: React.FC<React.ComponentProps<typeof Col>> = (props) => {
+  return <Col {...props} />
+}
+
+WindCol.displayName = 'WindDataFieldCol'
+
+export const SFieldItem = styled(WindCol)`
   margin: 0 0 8px 0;
   min-height: 20px;
   line-height: 20px;
 `
 
-export const SFieldLabel = styled(Col)`
+export const SFieldLabel = styled(WindCol)`
   color: #555;
 `
 
-export const SFieldValue = styled(Col)`
+export const SFieldValue = styled(WindCol)`
   color: #333;
 `
