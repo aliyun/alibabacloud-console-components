2edb1ef29966d2770b5d290ec726f298471abc4f
csr632 <632882184@qq.com>
2020-06-16 10:42:05 +0800

    save work

--- demo-workspaces/vite-mdx-dev/vite.config.ts
+++ demo-workspaces/vite-mdx-dev/vite.config.ts
@@ -12,10 +12,7 @@ import {
   loadDemoCode,
 } from '@alicloud/console-components-build-doc'
 import replace from '@rollup/plugin-replace'
-import * as fs from 'fs-extra'
-import { findPages } from './findPages.ts'
-import { resolvePageFile } from './resolvePageFile.ts'
-import { resolvePageConfig } from './resolvePageConfig.ts'
+import reactPages from 'vite-plugin-react-pages'
 
 const pagesDirPath = path.join(__dirname, 'pages')
 
@@ -32,7 +29,6 @@ module.exports = {
   },
   alias: {
     '/@pages/': pagesDirPath,
-    '/@infra/': path.join(__dirname, 'infra'),
   },
   plugins: [
     react,
@@ -51,46 +47,13 @@ module.exports = {
       ],
       rehypePlugins: [rehypeSlug],
     }),
+    reactPages({
+      pagesDir: pagesDirPath,
+    }),
   ],
   configureServer: ({ app, root, resolver }) => {
     app.use(async (ctx, next) => {
-      if (ctx.path === '/proxy-module' && ctx.query.path) {
-        const resolvedFilePath = await resolvePageFile(
-          ctx.query.path,
-          pagesDirPath
-        )
-        if (!resolvedFilePath || !fs.existsSync(resolvedFilePath)) {
-          ctx.status = 404
-          return
-        }
-        const actualModulePath = resolver.fileToRequest(resolvedFilePath)
-        ctx.body = `export { default } from "${actualModulePath}";
-export * from "${actualModulePath}";`
-        ctx.type = 'js'
-      } else if (ctx.path === '/page-config' && ctx.query.path) {
-        const pageConfigs = await resolvePageConfig(
-          ctx.query.path,
-          pagesDirPath
-        )
-        const pageConfigImportExp = pageConfigs
-          .map((v) => resolver.fileToRequest(v))
-          .map((publicPath, idx) => {
-            const varName = `config${idx}`
-            return (
-              `import ${varName} from "${publicPath}";\n` +
-              `configs.push(${varName});`
-            )
-          })
-          .join('\n')
-        ctx.body = `export const configs = [];\n${pageConfigImportExp}`
-        ctx.type = 'js'
-      } else if (ctx.path === '/api/pages') {
-        ctx.body = {
-          pages: await findPages(pagesDirPath),
-        }
-        ctx.status = 200
-        await next()
-      } else if ('loadDemo' in ctx.query) {
+      if ('loadDemo' in ctx.query) {
         const demoPath = resolver.requestToFile(ctx.path)
         console.log('demoPath', demoPath)
         const data = await loadDemoCode(demoPath, () => true)
