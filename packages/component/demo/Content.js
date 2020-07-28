e2e81694ce685ce834685084536d2c7cecf72b3d
csr632 <632882184@qq.com>
2020-07-28 19:16:46 +0800

    chore(component): fix dev env

--- packages/component/demo/Content.js
+++ packages/component/demo/Content.js
@@ -4,21 +4,28 @@ import Loadable from 'react-loadable'
 import { pascalCase } from './utils'
 import './content.scss'
 
-const Loading = ({
-  error,
-}) => (
-  error ? <div>Error</div> : <div>Loading</div>
-)
+const Loading = ({ error }) => (error ? <div>Error</div> : <div>Loading</div>)
+
+const ctx = require.context('../src/components/', true, /demo\/index\.[jt]sx?/)
+
+const keys = ctx.keys()
+
+function loader(component) {
+  const res = ctx(
+    keys.find((key) => {
+      if (key.includes(`${component}/demo/index`)) return true
+    })
+  )
+  return Promise.resolve(res)
+}
 
 Loading.propTypes = {
   error: PropTypes.objectOf(PropTypes.any),
 }
 
-const AppContent = ({
-  match,
-}) => {
+const AppContent = ({ match }) => {
   const Loader = Loadable({
-    loader: () => import(`../src/components/${match.params.component}/demo`),
+    loader: () => loader(match.params.component),
     loading: Loading,
   })
   return (
