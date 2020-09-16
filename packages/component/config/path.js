fa0958fddd54b8a1ceab38870fb87b052800d87f
csr632 <632882184@qq.com>
2020-09-16 11:38:01 +0800

    feat(component): use css from @alife/alicloud-components

--- packages/component/config/path.js
+++ packages/component/config/path.js
@@ -7,8 +7,11 @@ exports.DEMO = resolveCwd('demo')
 exports.DEMO_INDEX_FILE = resolveCwd('demo', 'index.js')
 exports.SRC = resolveCwd('src')
 exports.SRC_INDEX_FILE = resolveCwd('src', 'index.js')
-exports.SRC_SASS_INDEX_FILE = resolveCwd('src', 'index.scss')
-exports.SRC_SASS_NORESET_INDEX_FILE = resolveCwd('src', 'index-noreset.scss')
+// exports.SRC_SASS_INDEX_FILE = resolveCwd('src', 'index.scss')
+exports.SRC_SASS_INDEX_FILE = '@alife/alicloud-components/dist/xconsole.css'
+// exports.SRC_SASS_NORESET_INDEX_FILE = resolveCwd('src', 'index-noreset.scss')
+exports.SRC_SASS_NORESET_INDEX_FILE =
+  '@alife/alicloud-components/dist/xconsole-no-reset.css'
 exports.BUNDLE = resolveCwd('dist')
 exports.LIB = resolveCwd('lib')
 exports.NODE_MODULES = resolveCwd('node_modules')
