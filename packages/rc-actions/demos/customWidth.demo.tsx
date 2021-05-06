b28f043dca308f6f6772d89070ee7fa55a977213
csr632 <632882184@qq.com>
2021-05-06 21:20:44 +0800

    docs(rc-actions): improve docs

--- packages/rc-actions/demos/customWidth.demo.tsx
+++ packages/rc-actions/demos/customWidth.demo.tsx
@@ -10,7 +10,7 @@ const Basic: React.FC<{}> = () => {
   return (
     <Container>
       <div className="block">
-        <p>下拉Menu内置最大的宽度为150px：</p>
+        <p>下拉Menu内置最大的宽度为200px：</p>
         <Actions>
           <LinkButton
             onClick={() => {
@@ -33,7 +33,7 @@ const Basic: React.FC<{}> = () => {
               alert('on click')
             }}
           >
-            释放释放释放释放释放释放
+            释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放
           </LinkButton>
           <LinkButton
             // disabled
@@ -47,7 +47,7 @@ const Basic: React.FC<{}> = () => {
       </div>
       <div className="block">
         <p>开发者可通过传入menuProps来自定义最大宽度</p>
-        <Actions menuProps={{ style: { maxWidth: '100px' } }}>
+        <Actions menuProps={{ style: { maxWidth: '600px' } }}>
           <LinkButton
             onClick={() => {
               alert('on click')
@@ -69,7 +69,7 @@ const Basic: React.FC<{}> = () => {
               alert('on click')
             }}
           >
-            释放释放释放释放释放释放
+            释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放释放
           </LinkButton>
           <LinkButton
             // disabled
