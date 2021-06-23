c32f302cd4eec134febb53a99ba648765e2ceb79
csr632 <632882184@qq.com>
2021-06-23 15:41:15 +0800

    chore: update var generation

--- packages/rc-console-menu/scripts/gen-css-var.js
+++ packages/rc-console-menu/scripts/gen-css-var.js
@@ -9,15 +9,17 @@ var themes = {
 }
 
 fs.ensureDirSync(path.join(__dirname, `../dist/vars`))
+const version = fs.readJSONSync(path.join(__dirname, '../package.json')).version
 
 Object.entries(themes).forEach(([themeName, theme]) => {
   const style = theme.theme.values()
   const code = `:root {
+--console-menu-version: "${version}";
 ${Object.entries(style)
   .map(([propName, propValue]) => {
-    return `${propName}: ${propValue}`
+    return `${propName}: ${propValue};`
   })
-  .join(';\n')}
+  .join('\n')}
 }
 `
   const outPath = path.join(__dirname, `../dist/vars/${themeName}.css`)
