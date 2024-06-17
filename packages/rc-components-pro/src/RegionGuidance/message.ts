97514ae00be6d4efcb0c6eda5874d3c7f516886e
yr247308 <yr247308@alibaba-inc.com>
2024-06-17 14:54:08 +0800

    chore: update messages

--- packages/rc-components-pro/src/RegionGuidance/message.ts
+++ packages/rc-components-pro/src/RegionGuidance/message.ts
@@ -4,16 +4,16 @@ const isSSR = typeof document === 'undefined'
 
 const messages = {
   zh: {
-    nodata: '您在当前地域 {value} 暂无资源',
+    nodata: '您在当前地域 {value} 暂无实例',
     comma: '，',
     period: '',
-    switch: '您可以切换至以下有资源的地域',
+    switch: '您可以切换至以下有实例的地域',
     switch_suf: '',
     unit: '个',
     global_search_help: '没有找到想要的实例？',
   },
   en: {
-    nodata: 'You have no resources available in the selected region {value}',
+    nodata: 'You have no instance available in the selected region {value}',
     comma: ', ',
     period: '',
     switch: 'You can switch to the following regions: ',
@@ -29,10 +29,10 @@ const messages = {
     global_search_help: 'インスタンスが見つからない場合',
   },
   'zh-TW': {
-    nodata: '您在當前地區 {value} 暫時無數據',
+    nodata: '您在當前地區 {value} 暫時無個體',
     comma: ', ',
     period: '',
-    switch: '您可以切換至以下有資源的地區：',
+    switch: '您可以切換至以下有個體的地區：',
     unit: ' 個',
     global_search_help: '沒有找到想要的執行個體？',
   },
