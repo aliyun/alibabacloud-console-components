2ff5e0dba4a4a7819c44790add60caeef1b6c7ba
yr247308 <yr247308@alibaba-inc.com>
2024-07-01 14:23:58 +0800

    chore

--- packages/rc-components/rc-components-pro/src/types/IRegionGuidanceProps.type.ts
+++ packages/rc-components/rc-components-pro/src/types/IRegionGuidanceProps.type.ts
@@ -12,7 +12,8 @@ export interface IRegionGuidanceProps {
    */
   currentRegion: string;
   /**
-   * 当前地域名称。 注：可以不传，如果不传就会取 `ALIYUN_CONSOLE_CONFIG.REGIONS` 里面的名称，如果还是没找到就直接设置成 region id
+   * 当前地域名称。 注：可以不传，如果不传就会取 `ALIYUN_CONSOLE_CONFIG.REGIONS` 里面的名称
+   * 如果还是没找到就直接设置成 regionId
    */
   currentRegionName?: string;
   /**
