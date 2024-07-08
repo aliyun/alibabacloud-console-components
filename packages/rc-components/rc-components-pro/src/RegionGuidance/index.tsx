3e1dad2e3d4388ef4e954dfd01d5fbbc81a752aa
yr247308 <yr247308@alibaba-inc.com>
2024-07-08 21:12:49 +0800

    fix(pro): fix regionListLabel

--- packages/rc-components/rc-components-pro/src/RegionGuidance/index.tsx
+++ packages/rc-components/rc-components-pro/src/RegionGuidance/index.tsx
@@ -9,63 +9,63 @@ import LoggerProvider from '../Provider/LoggerProvider';
 
 const getRegionName = (regionId: string) => {
   // @ts-ignore
-  const REGIONS = window?.ALIYUN_CONSOLE_CONFIG?.REGIONS || []
+  const REGIONS = window?.ALIYUN_CONSOLE_CONFIG?.REGIONS || [];
   const region = REGIONS.find((t: any) => {
-    return t.regionId === regionId
+    return t.regionId === regionId;
   });
 
   return region?.name || regionId;
-}
+};
 
-const FlattenRegionList = ({regionList = [], onRegionClick}: IRegionGuidanceProps) => {
+const FlattenRegionList = ({ regionList = [], onRegionClick }: IRegionGuidanceProps) => {
   const list = regionList.map((region, index) => (
     <span key={region.id}>
       <a
         className="xconsole-rc-region-anchor"
-        onClick={(value) => {onRegionClick && onRegionClick(region.id)}}
+        onClick={(value) => { onRegionClick && onRegionClick(region.id); }}
       >
-          {region.name || getRegionName(region.id)}
+        {region.name || getRegionName(region.id)}
       </a>
       <span className="xconsole-rc-region-count">
         {region.count || 0}
       </span>{ index !== regionList.length - 1 ? '、' : ''}
     </span>));
-  return <>{list}</>
-}
+  return <>{list}</>;
+};
 
-const DropDownRegionList = ({regionList = [], onRegionClick, intl}: IRegionGuidanceProps) => {
+const DropDownRegionList = ({ regionList = [], onRegionClick, intl }: IRegionGuidanceProps) => {
   const [iconType, setIconType] = React.useState('caret-down');
   return (
     <Dropdown
-      trigger={<a className="xconsole-rc-region-anchor"> {regionList.length} {intl('unit')} <Icon type={iconType} size="xs"/></a>}
+      trigger={<a className="xconsole-rc-region-anchor"> {regionList.length} {intl('unit')} <Icon type={iconType} size="xs" /></a>}
       triggerType={['click', 'hover']}
       beforeOpen={() => setIconType('caret-up')}
       beforeClose={() => setIconType('caret-down')}
     >
-      <Menu style={{maxHeight: 320, overflowY: 'scroll'}} onItemClick={(value) => {onRegionClick && onRegionClick(value)}}>
+      <Menu style={{ maxHeight: 320, overflowY: 'scroll' }} onItemClick={(value) => { onRegionClick && onRegionClick(value); }}>
         { regionList.map(region => (
-            <Menu.Item key={region.id}>
-              <span>
-                {region.name || getRegionName(region.id)}
-                <span className="xconsole-rc-region-item-count">
-                  {region.count || 0}
-                </span>
+          <Menu.Item key={region.id}>
+            <span>
+              {region.name || getRegionName(region.id)}
+              <span className="xconsole-rc-region-item-count">
+                {region.count || 0}
               </span>
-            </Menu.Item>
-          ))
+            </span>
+          </Menu.Item>
+        ))
         }
       </Menu>
     </Dropdown>
-  )
-}
+  );
+};
 
 const RegionList = (props: IRegionGuidanceProps) => {
-  const { regionList = []} = props;
+  const { regionList = [] } = props;
   if (regionList.length < 3) {
-    return <FlattenRegionList {...props}/>;
+    return <FlattenRegionList {...props} />;
   }
-  return <DropDownRegionList {...props}/>;
-}
+  return <DropDownRegionList {...props} />;
+};
 
 const RegionGuidance = (props: IRegionGuidanceProps) => {
   const {
@@ -76,42 +76,43 @@ const RegionGuidance = (props: IRegionGuidanceProps) => {
     regionListLabel,
     globalSearchAction,
     type,
-    intl
+    intl,
   } = props;
 
   if (type === 'message') {
     return (
       <Message
-        type='notice'
+        type="notice"
         size="medium"
         title={(
           <div className="xconsole-rc-region-message-content">
-            { children ? children : <span>{intl('nodata', {value: currentRegionName || getRegionName(currentRegion)})}{regionList?.length? intl('comma'): intl('period')}</span> }
-            {regionList?.length ? <span>{regionListLabel || intl('switch')} <RegionList {...props}/> </span> : null}
+            { children || <span>{intl('nodata', { value: currentRegionName || getRegionName(currentRegion) })}{regionList?.length ? intl('comma') : intl('period')}</span> }
+            {regionList?.length ? <span>{regionListLabel || intl('switch')} <RegionList {...props} /> </span> : null}
           </div>
         )}
       />
-    )
+    );
   }
 
   return (
     <div className="xconsole-rc-region-guidance">
       <LoggerProvider regionId={currentRegion} componentName="RegionGuidance" />
-      { children ? children : <div>{intl('nodata', {value: currentRegionName || getRegionName(currentRegion)})}</div>}
-      
-      { regionList?.length ? <div>{intl('switch')} <RegionList {...props}/> </div> : null}
+      { children || <div>{intl('nodata', { value: currentRegionName || getRegionName(currentRegion) })}</div>}
+
+      { regionList?.length ? <div>{regionListLabel || intl('switch')} <RegionList {...props} /> </div> : null}
 
       { globalSearchAction
-        ? (<div>
-          <a className="xconsole-rc-region-anchor">
-            <Icon type="help" style={{ verticalAlign: 'bottom' }} size={"xs"}/> {intl('global_search_help')}
-          </a>
-        </div>)
-        : null
+        ? (
+          <div>
+            <a className="xconsole-rc-region-anchor">
+              <Icon type="help" style={{ verticalAlign: 'bottom' }} size={'xs'} /> {intl('global_search_help')}
+            </a>
+          </div>
+        ) : null
       }
     </div>
   );
-}
+};
 
 export const RegionGuidanceWithIntl = withRcIntl({
   componentName: 'RegionGuidance',
