2c4bf4876156fbe0902652c23127c83d160ed23c
yr247308 <yr247308@alibaba-inc.com>
2025-02-28 12:41:17 +0800

    chore: remove intl

--- packages/misc/intl-context/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Provider from './Provider'
-import Consumer from './Consumer'
-import Context from './Context'
-
-export { Provider, Consumer, Context }
-export * from './types'
--- packages/misc/intl-core/src/react.ts
+++
@@ -1,3 +0,0 @@
-// keep this file for backward compatibility reason
-// user may be using `import reactIntl from '@alicloud/console-components-intl/react'`
-export { default } from './index'
