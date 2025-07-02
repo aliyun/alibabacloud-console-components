d7a0c85bf05c09702696025737e8acc34b2d575b
yr247308 <yr247308@alibaba-inc.com>
2025-07-02 14:26:30 +0800

    fix(table): support column-group

--- packages/rc-components/rc-table/src/renderColumns.tsx
+++ packages/rc-components/rc-table/src/renderColumns.tsx
@@ -1,32 +1,47 @@
-import React from 'react'
-import get from 'lodash/get'
-import { wrapDisplayName } from 'recompose'
+import React from 'react';
+import get from 'lodash/get';
+import { wrapDisplayName } from 'recompose';
 import {
   TableProps,
   ColumnProps,
-} from '@alifd/next/types/table'
-import { Table } from '@alifd/next'
-import { ITableProps } from './layout'
+} from '@alifd/next/types/table';
+import { Table } from '@alifd/next';
+import { ITableProps } from './layout';
 
-const getColumnKeyByIndex = (index: number): string =>
-  `wind-table-column-${index}`
+const getColumnKeyByIndex = (index: number): string => `wind-table-column-${index}`;
 
 const getColumnKey = (
   column: ColumnProps,
   primaryKey: TableProps['primaryKey'],
-  index: number
-): string =>
-  primaryKey
-    ? get(column, primaryKey, getColumnKeyByIndex(index))
-    : getColumnKeyByIndex(index)
+  index: number,
+): string => (primaryKey
+  ? get(column, primaryKey, getColumnKeyByIndex(index))
+  : getColumnKeyByIndex(index));
 
 const toComponent = (primaryKey: TableProps['primaryKey']) => (
   column: ColumnProps,
-  i: number
-) => <Table.Column key={getColumnKey(column, primaryKey, i)} {...column} />
+  i: number,
+) => {
+  if (column.children) {
+    return (
+      // @ts-ignore
+      <Table.ColumnGroup title={column.title} align={column.align}>
+        {
+          (column.children as ColumnProps[]).map((col, index) => {
+            return toComponent(
+              (col as { primaryKey?: ITableProps['primaryKey'] }).primaryKey,
+            )(col, index);
+          })
+        }
+      </Table.ColumnGroup>
+    );
+  }
+
+  return <Table.Column key={getColumnKey(column, primaryKey, i)} {...column} />;
+};
 
 function renderColumns<T>(
-  WrappedComponent: React.ComponentType<T>
+  WrappedComponent: React.ComponentType<T>,
 ): React.FC<T & { columns: ITableProps['columns'] }> {
   const H: React.FC<T & { columns: ITableProps['columns'] }> = ({
     columns = [],
@@ -35,16 +50,16 @@ function renderColumns<T>(
     return (
       <WrappedComponent {...(restProps as T)}>
         {columns.map(
-          toComponent(
-            (restProps as { primaryKey?: ITableProps['primaryKey'] }).primaryKey
-          )
+          (toComponent)(
+            (restProps as { primaryKey?: ITableProps['primaryKey'] }).primaryKey,
+          ),
         )}
         {restProps.children}
       </WrappedComponent>
-    )
-  }
-  H.displayName = wrapDisplayName(WrappedComponent, 'renderColumns')
-  return H
+    );
+  };
+  H.displayName = wrapDisplayName(WrappedComponent, 'renderColumns');
+  return H;
 }
 
-export default renderColumns
+export default renderColumns;
